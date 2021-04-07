f0df12ebf6cea7cd9040dbc3774ec386d8ea4e67
Sebastian Castro <scastro@condorlabs.io>
2021-04-07 15:51:36 -0500

    Changed files-config.sh

--- files-config.sh
+++ files-config.sh
@@ -12,7 +12,7 @@ if [ -f .prettier.config.js ];then rm .prettier.config.js;fi
 if [ -f .prettier.config.json ];then rm .prettier.config.json;fi
 npm i eslint@7.23.0 @scastro37/eslint-config -D
 ESLINT='module.exports = {extends: ["@scastro37/eslint-config/configTS"]};'
-PRETTIER='"@scastro37/prettier-config";'
+PRETTIER='"@scastro37/prettier-config"'
 SETTINGS='"[javascript]": {
     "editor.defaultFormatter": "esbenp.prettier-vscode",
     "editor.formatOnSave": true
@@ -21,8 +21,8 @@ SETTINGS='"[javascript]": {
     "editor.defaultFormatter": "esbenp.prettier-vscode",
     "editor.formatOnSave": true
   }'
-printf "${PRETTIER} %s\n" > .jr-prettierrc
-printf "${ESLINT} %s\n" > .jr-eslint.js
+printf "${PRETTIER} %s\n" > .prettierrc
+printf "${ESLINT} %s\n" > .eslint.js
 #Config setting.json
 mkdir ../.vscode
 printf "${SETTINGS} %s\n" > .vscode/settings.json
