fd4dc42f2b6d33be60cc073c449925835cebcb5a
Sebastian Castro <scastro@condorlabs.io>
2021-04-08 16:34:50 -0500

    Changed mrm config

--- mrm-config/index.js
+++ mrm-config/index.js
@@ -1,7 +1,8 @@
-const { packageJson } = require("mrm-core");
-
+const { packageJson } = require('mrm-core');
 packageJson()
         .removeScript('prepare')
+        .unset('lint-staged')
         .setScript('lint', 'eslint . --fix')
         .setScript('format', 'prettier . --write')
+        .set('lint-staged',{'*.{js,jsx,ts,tsx}': ['npm run format', 'npm run lint']})
         .save()
