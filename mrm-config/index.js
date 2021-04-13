a1f2784b86727be95d2819ce642d62aa15479460
Sebastian Castro <scastro@condorlabs.io>
2021-04-13 15:20:34 -0500

    Added ignore

--- mrm-config/index.js
+++ mrm-config/index.js
@@ -1,10 +1,10 @@
-const { packageJson, json } = require('mrm-core');
+const { packageJson, json, lines } = require('mrm-core');
 packageJson()
   .removeScript('prepare')
   .unset('lint-staged')
   .setScript('lint', 'eslint --fix')
   .setScript('format', 'prettier --write')
-  .setScript('lint-global', 'eslint . --fix && prettier . --write')
+  .setScript('lint-global', 'echo "Prettier is running..." && prettier . --write && echo "ESlint is running" && eslint . --fix')
   .set('lint-staged', { '*.{js,jsx,ts,tsx}': ['npm run lint', 'npm run format'] })
   .save();
 
@@ -20,3 +20,7 @@ json('.vscode/settings.json')
         }
       })
   .save();
+
+const ignore = ['public', 'node_modules', 'dist', 'lib'];  
+lines('.prettierignore').set(ignore).save();
+lines('.eslintignore').set(ignore).save();
