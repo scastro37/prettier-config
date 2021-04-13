5ad84269b4ae788d95c920f96c07621abe89e364
Sebastian Castro <scastro@condorlabs.io>
2021-04-12 19:10:49 -0500

    Solved config

--- mrm-config/index.js
+++ mrm-config/index.js
@@ -1,8 +1,21 @@
-const { packageJson } = require('mrm-core');
+const { packageJson, json } = require('mrm-core');
 packageJson()
-        .removeScript('prepare')
-        .unset('lint-staged')
-        .setScript('lint', 'eslint --fix')
-        .setScript('format', 'prettier --write')
-        .set('lint-staged',{'*.{js,jsx,ts,tsx}': ['npm run lint','npm run format']})
-        .save()
+  .removeScript('prepare')
+  .unset('lint-staged')
+  .setScript('lint', 'eslint --fix')
+  .setScript('format', 'prettier --write')
+  .set('lint-staged', { '*.{js,jsx,ts,tsx}': ['npm run lint', 'npm run format'] })
+  .save();
+
+json('.vscode/settings.json')
+  .merge({
+        "[typescript]": {
+          "editor.defaultFormatter": "esbenp.prettier-vscode",
+          "editor.formatOnSave": true
+        },
+        "[javascript]": {
+          "editor.defaultFormatter": "esbenp.prettier-vscode",
+          "editor.formatOnSave": true
+        }
+      })
+  .save();
--- mrm-uninstall/index.js
+++ mrm-uninstall/index.js
@@ -1,6 +1,5 @@
-const { packageJson } = require('mrm-core');
-packageJson()
-        .removeScript('lint')
-        .removeScript('format')
-        .unset('lint-staged')
-        .save()
+const { packageJson, json } = require('mrm-core');
+
+packageJson().removeScript('lint').removeScript('format').unset('lint-staged').save();
+
+json('.vscode/settings.json').unset('[javascript]').unset('[typescript]').save();
