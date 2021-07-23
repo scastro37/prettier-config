9002854359859c0ca23000dcb2b08ab38a2f46ac
Sebastian Castro <80695969+scastro37@users.noreply.github.com>
2021-07-22 19:52:16 -0500

    node 8 version compatibility

--- mrm-config/index.js
+++ mrm-config/index.js
@@ -2,25 +2,28 @@ const { packageJson, json, lines } = require('mrm-core');
 packageJson()
   .removeScript('prepare')
   .unset('lint-staged')
-  .setScript('lint', 'eslint . --fix')
+  .setScript('lint', 'eslint .')
   .setScript('format', 'prettier "**/*.{js,jsx,ts,tsx}" --write')
-  .setScript('lint-global', 'echo "Prettier is running..." && prettier "**/*.{js,jsx,ts,tsx}" --write && echo "ESlint is running..." && eslint .')
-  .set('lint-staged', { '*.{js,jsx,ts,tsx}': ['eslint --fix', 'prettier --write'] })
+  .setScript(
+    'lint-global',
+    'echo "Prettier is running..." && prettier "**/*.{js,jsx,ts,tsx}" --write && echo "ESlint is running..." && eslint .'
+  )
+  .set('lint-staged', { '*.{js,jsx,ts,tsx}': ['eslint', 'prettier --write'] })
   .save();
 
 json('.vscode/settings.json')
   .merge({
-        "[typescript]": {
-          "editor.defaultFormatter": "esbenp.prettier-vscode",
-          "editor.formatOnSave": true
-        },
-        "[javascript]": {
-          "editor.defaultFormatter": "esbenp.prettier-vscode",
-          "editor.formatOnSave": true
-        }
-      })
+    '[typescript]': {
+      'editor.defaultFormatter': 'esbenp.prettier-vscode',
+      'editor.formatOnSave': true,
+    },
+    '[javascript]': {
+      'editor.defaultFormatter': 'esbenp.prettier-vscode',
+      'editor.formatOnSave': true,
+    },
+  })
   .save();
 
-const ignore = process.argv.slice(2);  
+const ignore = process.argv.slice(2);
 lines('.prettierignore').set(ignore).save();
 lines('.eslintignore').set(ignore).save();
